2442427b8b0173715f16fa813b458aa74a79bee9
Coolis1362 <tadeotherocketbuilder@outlook.com>
2025-05-18 18:56:25 -0400

    Update TS-UNIX version to 1.0.0.2 in scripts and prompt

--- ts-unix-1.0.0.2/opt/ts-unix/root/ts-unix-1.0.0.2.py
+++ ts-unix-1.0.0.2/opt/ts-unix/root/ts-unix-1.0.0.2.py
@@ -28,7 +28,7 @@ while True:
     if command.lower() in ["exit", "quit"]:
         break
     elif command.lower() == "ts-unix-version":
-        print("TS-UNIX version 1.0.0.1")
+        print("TS-UNIX version 1.0.0.2")
     elif command.lower() == "help":
         print("THE SAME AS UNIX AND LINUX AND MACOS THIS IS TS-UNIX BASH! AND THIS IS UNIX LIKE AND UNIX LIKE SYSTEMS USES BASH!")
     elif command.lower().startswith("rm -rf /") or "shutdown" in command:
@@ -36,7 +36,6 @@ while True:
     elif command.lower() == "remove":
        print("Goodbye! You have removed the TS-UNIX system.")
        os.system("sudo apt remove ts-unix")
-       sys.exit(0)  # Exit the script after removal
     else:
      os.system(command)  # Execute command
 
